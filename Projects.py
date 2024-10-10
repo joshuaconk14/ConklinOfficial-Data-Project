6ef01c60efea2a6f78949c630b226e8192cba210
Joshua Conklin <joshuaconk14>
2024-10-09 22:10:08 -0700

    bravo add

--- Projects.py
+++ Projects.py
@@ -32,6 +32,20 @@ def app():
     #Project 1
     with st.container():
         image_url = "pictures/Bravo_company_pic_copy.png"
+        col1, col2 = st.columns([1,2])
+        with col1:
+            st.image(image_url, width = 250)
+        with col2:
+            st.subheader("BravoTekk - Mobile App Front-End Development")
+            st.write("(App currently being built)")
+            streamlit_url = "https://github.com/joshuaconk14/Tekk_UI_Project.git"
+            st.markdown(f"[Visit Github Repo]({streamlit_url})")
+    st.write("September 2024 - Present")
+    st.write("Use Xcode, SwiftUI, and Rive to build app interface for sports training mobile application.")
+    st.write("")
+    st.write("Skills: SwiftUI, Xcode, Rive, User Expereince (UX), User Interface Design, Software Design, Animation")
+    st.write("##")
+    st.write("##")
 
     #Project 2
     with st.container():
@@ -52,8 +66,8 @@ def app():
                 #st.image(image_url, width = 900)
             streamlit_url = "https://github.com/joshuaconk14/Data_Jobs_Project.git"
             st.markdown(f"[Visit Github Repo]({streamlit_url})")
-    st.write("August 2024")
-    st.write("Used SQL and Tableau in order to determine which programming languages were mentioned the most in job descriptions for data roles (mainly Data Engineers, Data Analysts, and Data Scientists). This is to help data job seekers know what programming languages they need to understand and practice for specific data jobs.")
+    st.write("August 2024 - September 2024")
+    st.write("Used SQL and Tableau in order to determine which programming languages were mentioned the most in job descriptions for data roles. Helps data job seekers know what languages they need to practice for specific data jobs.")
     st.write("")
     st.write("Skills: SQL, Tableau, Azure Studio, Databases, Storytelling, Data Visualization, Big Data, Data Connection, Performance Optimization")
     st.write("##")
@@ -67,12 +81,12 @@ def app():
         with col1:
             st.image(image_url, width = 250)
         with col2:
-            st.subheader("ConklinOfficial KPI Dashboard - Relationship between KPIs and Content")
+            st.subheader("ConklinOfficial KPIs Dashboard")
             st.markdown("[View Full Project Display](https://co-kpis-project.streamlit.app/)")
             streamlit_url = "https://github.com/joshuaconk14/CO_KPIs_Project.git"
             st.markdown(f"[Visit Github Repo]({streamlit_url})")
     st.write("Jul 2024 - Aug 2024")
-    st.write("Used Python, Pandas, and Numpy demonstrate the relationship between KPIs and the type of reel posted, indicating what type of post performs best and which posts posts we should focus on.")
+    st.write("Extracted KPI data from ConklinOfficial Instagram page into Python, creating data story correlating the relationship between KPIs and the type of content posted.")
     st.write("")
     st.write("Skills: Python, Jupyter, Pandas, Numpy, HTML, Data Extraction, Data Visualization, Software Design, Data Analysis")
     st.write("##")
@@ -90,9 +104,9 @@ def app():
             streamlit_url = "https://github.com/joshuaconk14/Qualtrics_Survey_Project.git"
             st.markdown(f"[Visit Github Repo]({streamlit_url})")
     st.write("Feb 2024 - May 2024")
-    st.write("Conducted a survey through Qualtrics platform, create with Rstudio, to research people's opinions on influencer marketing affecting consumer purchasing behavior. Coded the data and analyze the participant response trends in order to make future predictions.")
+    st.write("Used Rstudio to create and conduct a survey through Qualtrics. Data analysis on people's opinions on influencer marketing affecting consumer purchasing behavior.")
     st.write("")
-    st.write("Skills: R, Excel, Data Analysis, Research Design, Qualitative Research, Business Analytics, Data Entry")
+    st.write("Skills: R, Excel, Data Analysis, Research Design, Qualitative Research, Business Analytics, Microsoft Excel, Data Entry")
     st.write("##")
     st.write("##")
 
@@ -107,6 +121,6 @@ def app():
             streamlit_url = "https://www.fremontyouthsoccer.com/2017/02/the-new-fremont-youth-soccer-club-badge/"
             st.markdown(f"[View Project Announcement]({streamlit_url})")
     st.write("Jan 2017 - Feb 2017")
-    st.write("Created FYSC official logo with Adobe Illustrator that is being used today. Creatively incorporated club values and worked with board members presenting multiple ideas to create the best logo possible.")
+    st.write("Created FYSC official logo that is being used today. Creatively incorporated club values onto the logo using Adobe Illustrator.")
     st.write("")
     st.write("Skills: Adobe Illustrator, Graphic Design, Creativity Skills")
